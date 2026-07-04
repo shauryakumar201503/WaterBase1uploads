57689f8259dfa7d6fe9b9d7166044c1df36d3cad
shauryakumar201503 <shauryakumar201503@gmail.com>
2026-07-04 18:08:54 +0100

    Add navigation buttons to frontpage.js

---
+++ accounts/frontpage.js
@@ -0,0 +1,11 @@
+document.getElementById("loginBtn").onclick = function () {
+    window.location.href = "login.html";
+};
+
+document.getElementById("createBtn").onclick = function () {
+    window.location.href = "create.html";
+};
+
+document.getElementById("dashboardBtn").onclick = function () {
+    window.location.href = "dashboardbuttons.html";
+};
